b36554aee827176ccb7d723aa8a8e0d112057aa0
Joshua Li <54961726+joshli0@users.noreply.github.com>
2024-02-13 21:11:48 -0500

    Add recommender_app.py

---
+++ recommender_app.py
@@ -0,0 +1,34 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import streamlit as st
+
+#set up Spotify credentials
+client_id = "CLIENT ID" 
+client_secret = "CLIENT SECRET"
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def get_recommendations(track_name):
+    #get track URI
+    results = sp.search(q=track_name, type='track')
+    track_uri = results['tracks']['items'][0]['uri']
+
+    #get recommended tracks
+    recommendations = sp.recommendations(seed_tracks=[track_uri])['tracks']
+    return recommendations
+
+st.title("Music Recommender")
+
+track = st.text_input('Enter a song name: ')
+if track:
+    init_track = sp.search(q=track, type = 'track')
+    st.write(init_track['tracks']['items'][0]['name'] + ' by ' + init_track['tracks']['items'][0]['artists'][0]['name'])
+    st.image(init_track['tracks']['items'][0]['album']['images'][0]['url'], width = 200)
+    st.divider()
+    st.header('Recommended Songs:')
+    for i in get_recommendations(track):
+        st.subheader(i['name'])
+        st.write(' by ' + i['artists'][0]['name'])
+        st.link_button("Link to song", i['external_urls']['spotify'])
+        st.image(i['album']['images'][0]['url'], width = 400)
+        st.divider()
